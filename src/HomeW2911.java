a71a2e4fb7154489e7bbce026b0c67f756accd98
AIT TR Student <virese6@gmail.com>
2023-11-30 20:13:21 +0100

    Completed HW from 30.11. gg wp

--- src/HomeW2911.java
+++ src/HomeW2911.java
@@ -1,20 +1,24 @@
 public class HomeW2911 {
     public static void main(String[] args) {
-        int[] array = {10,10,10,20,3,5};
-        System.out.println(returnNumbers(array));
-
+        String word = "hello";//t1
+        System.out.println(returnDoubleNumbers(word));//t1
+        String cup = "tee";//t2
+        String glass = "cola";//t2
+        System.out.println(returnDoubleStrings(cup, glass));//t2
+        String lol = "man";//t3
+        System.out.println(change(lol));//t3
     }
-    public static boolean returnNumbers(int[] array){
-        int sum=0;
-        boolean result = true;
-        for (int i = 0; i < array.length; i ++){
-            if(array[i] == 10){
-                sum++;
-                if (sum == 3){
-                    return result;
-                }
-            }
-        }
-        return !result;
+    public static int returnDoubleNumbers(String word) {//t1
+    int doubleSum = word.length() * 2;
+        return doubleSum;
+    }
+    public static String returnDoubleStrings(String cup,String glass){//t2
+        String finish = cup.substring(1) + glass.substring(1);
+        return finish.toUpperCase();
+    }
+    public static String change(String lol){//t3
+        char ver = lol.charAt(0);
+        String lolNew = lol.substring(1);
+        return lolNew + ver;
     }
 }
