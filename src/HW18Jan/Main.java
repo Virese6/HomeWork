4fedf558a7b2870bd25f274b6b0d52b7ff65b5ae
AIT TR Student <virese6@gmail.com>
2024-01-19 09:43:44 +0100

    HW18Jan.new

--- src/HW18Jan/Main.java
+++ src/HW18Jan/Main.java
@@ -1,7 +1,9 @@
 package HW18Jan;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,8 +17,23 @@ public class Main {
         String nameToFind = "Ivan";
         System.out.println(returnNumber(names,nameToFind));
     }
-    public static int returnNumber(List<String> names, String nameToFind){
-        int count=0;
+    public static int returnNumber(List<String> names, String nameToFind) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String name : names) {
+            if (map.containsKey(name))
+                map.put(name, map.get(name) + 1);
+            else map.put(name, 1);
+        }
+        return map.containsKey(nameToFind) ? map.get(nameToFind) : 0;
+    }
+}
+
+/*
+Задача на мапы: Дан список имен, в котором некоторые имена повторяются.
+Написать метод, который по имени вернет количество вхождений этого имени в список.
+т.е. метод принимает лист и имя и возвращает число, соответствующее количеству раз,
+которое это имя встречается в листе
+int count=0;
         for (int i=0; i<names.size();i++){
             if(names.contains(nameToFind)){
                 count++;
@@ -25,12 +42,4 @@ public class Main {
             }
         return count;
         }
-
-    }
-
-/*
-Задача на мапы: Дан список имен, в котором некоторые имена повторяются.
-Написать метод, который по имени вернет количество вхождений этого имени в список.
-т.е. метод принимает лист и имя и возвращает число, соответствующее количеству раз,
-которое это имя встречается в листе
  */
